068c04255e49097e8f29366edf626d2e63edbb66
erick-gulyan <97641133+erick-gulyan@users.noreply.github.com>
2022-02-03 18:29:36 -0800

    changed 10 to 25
    now test is correct and should pass

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply(){
-        assertEquals(10,SkillDemo.multiply(5,5)); //this should fail since 5*5=25
+        assertEquals(25,SkillDemo.multiply(5,5)); //this should fail since 5*5=25
     }
 }
